1a0ab660cf537e2020a16b5e3fdc91b8186165a7
Wahidul30 <wahidul505@gmail.com>
2022-04-16 14:29:33 +0600

    implemented functionalities for login and register

--- src/Pages/Forms/Login/Login.js
+++ src/Pages/Forms/Login/Login.js
@@ -1,14 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Login = () => {
+    const [userInfo, setUserInfo] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    });
+    const [errors, setErrors] = useState({
+        emailError: "",
+        passwordError: "",
+        general: "",
+    })
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        loginError,
+    ] = useSignInWithEmailAndPassword(auth);
+    const handleEmailChange = e => {
+        setUserInfo({ ...userInfo, email: e.target.value });
+    }
+    const handlePasswordChange = e => {
+        setUserInfo({ ...userInfo, password: e.target.value });
+    }
+    const handleLogin = e => {
+        e.preventDefault();
+        signInWithEmailAndPassword(userInfo.email, userInfo.password);
+    }
+    useEffect(() => {
+        if (loginError) {
+            switch (loginError.code) {
+                case "auth/wrong-password":
+                    setErrors({ ...errors, general: "Wrong Password" });
+                    break;
+                case "auth/user-not-found":
+                    setErrors({ ...errors, general: "Invalid Email Address" });
+                    break;
+                default:
+                    setErrors({ ...errors, general: "Something went wrong" });
+            }
+        }
+        if (user) {
+            setErrors({ emailError: "", passwordError: "", general: "" });
+        }
+    }, [loginError, errors, user]);
     return (
         <div className='w-2/3 md:w-1/3 mx-auto'>
             <h1 className='text-white text-3xl font-semibold mb-6'>Login</h1>
-            <form className='flex flex-col gap-4 text-xl'>
-                <input className='bg-white py-1 px-2 rounded focus:outline-none' type="email" name="email" id="email" placeholder='Email' />
-                <input className='bg-white py-1 px-2 rounded focus:outline-none' type="password" name="password" id="password" placeholder='Password' />
-                <input className='bg-amber-500 text-white rounded py-1 px-2' type="submit" value="Login" />
+            <form onSubmit={handleLogin} className='flex flex-col gap-4 text-xl'>
+                <input onChange={handleEmailChange} className='bg-white py-1 px-2 rounded focus:outline-none' type="email" name="email" id="email" placeholder='Email' />
+                <input onChange={handlePasswordChange} className='bg-white py-1 px-2 rounded focus:outline-none' type="password" name="password" id="password" placeholder='Password' />
+                {errors?.general && <p className='text-red-400'>{errors?.general}</p>}
+                <input className='bg-amber-500 text-white rounded py-1 px-2 cursor-pointer' type="submit" value="Login" />
             </form>
             <div>
                 <SocialLogin />
--- src/Pages/Forms/Register/Register.js
+++ src/Pages/Forms/Register/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
@@ -10,8 +10,10 @@ const Register = () => {
         password: "",
         confirmPassword: ""
     });
-    const [error, setError] = useState({
-
+    const [errors, setErrors] = useState({
+        emailError: "",
+        passwordError: "",
+        general: "",
     })
     const [
         createUserWithEmailAndPassword,
@@ -23,10 +25,27 @@ const Register = () => {
         setUserInfo({ ...userInfo, name: e.target.value });
     }
     const handleEmailChange = e => {
-        setUserInfo({ ...userInfo, email: e.target.value });
+        const emailRegex = /\S+@\S+\.\S+/;
+        const validEmail = emailRegex.test(e.target.value);
+        if (validEmail) {
+            setUserInfo({ ...userInfo, email: e.target.value });
+            setErrors({ ...errors, emailError: "" })
+        } else {
+            setUserInfo({ ...userInfo, email: "" });
+            setErrors({ ...errors, emailError: "Please, Give a valid Email Address" });
+        }
+
     }
     const handlePasswordChange = e => {
-        setUserInfo({ ...userInfo, password: e.target.value });
+        const passwordRegex = /.{8,}$/;
+        const validPassword = passwordRegex.test(e.target.value);
+        if (validPassword) {
+            setUserInfo({ ...userInfo, password: e.target.value });
+            setErrors({ ...errors, passwordError: "" });
+        } else {
+            setUserInfo({ ...userInfo, password: "" });
+            setErrors({ ...errors, passwordError: "Password is too short" })
+        }
     }
     const handleConfirmPasswordChange = e => {
         setUserInfo({ ...userInfo, confirmPassword: e.target.value });
@@ -34,18 +53,36 @@ const Register = () => {
     const handleRegister = e => {
         e.preventDefault();
         if (userInfo.password !== userInfo.confirmPassword) {
+            setErrors({ ...errors, passwordError: "Password Didn't matched" });
             return;
         }
         createUserWithEmailAndPassword(userInfo.email, userInfo.password);
     }
+    useEffect(() => {
+        if (creatUserError) {
+            switch (creatUserError.code) {
+                case "auth/email-already-in-use":
+                    setErrors({ ...errors, general: "Email Already Exist" });
+                    break;
+                default:
+                    setErrors({ ...errors, general: "Something went wrong" });
+            }
+        }
+        if(user){
+            setErrors({emailError: "", passwordError: "", general:""});
+        }
+    }, [creatUserError, errors, user]);
     return (
-        <div className='w-2/3 md:w-1/3 mx-auto'>
+        <div className='w-2/3 md:w-1/3 mx-auto -mt-8'>
             <h1 className='text-white text-3xl font-semibold mb-6'>Create an Account</h1>
             <form onSubmit={handleRegister} className='flex flex-col gap-4 text-xl'>
                 <input onChange={handleNameChange} className='bg-white py-1 px-2 rounded focus:outline-none' type="text" name="name" id="name" placeholder='Name' />
                 <input onChange={handleEmailChange} className='bg-white py-1 px-2 rounded focus:outline-none' type="email" name="email" id="email" placeholder='Email' />
+                {errors?.emailError && <p className='text-red-400'>{errors?.emailError}</p>}
                 <input onChange={handlePasswordChange} className='bg-white py-1 px-2 rounded focus:outline-none' type="password" name="password" id="password" placeholder='Password' />
+                {errors?.passwordError && <p className='text-red-400'>{errors?.passwordError}</p>}
                 <input onChange={handleConfirmPasswordChange} className='bg-white py-1 px-2 rounded focus:outline-none' type="password" name="confirm_password" id="confirm_password" placeholder='Confirm Password' />
+                {errors?.general && <p className='text-red-400'>{errors?.general}</p>}
                 <input className='bg-amber-500 text-white rounded py-1 px-2 cursor-pointer' type="submit" value="Create Account" />
             </form>
             <div>
